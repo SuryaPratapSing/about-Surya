82e4f67314147ca6d46db670d1dc87787c00d165
surya <iamsingh.surya@gmail.com>
2024-04-18 15:47:38 +0530

    add Work.jsx component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import Contact from './Components/Contact/Contact';
 import Cursor from './Components/Cursor/Cursor';
-import Experiance from './Components/Experiance/Experiance';
 import Footer from './Components/Footer/Footer';
-import Hero from './Components/HeroSection/Hero';
-import Knowladge from './Components/Knowladge/Knowladge';
 import Nav from './Components/Navbar/Nav';
-import Section from './Components/Section/Section';
+import Main from './Components/Main';
+import { Route, Routes } from 'react-router-dom';
+import Work from './Components/Work/Work';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      // Calculate the current page based on scroll position
-      const pageHeight = window.innerHeight;
-      const currentPage = Math.floor(scrollPosition / pageHeight);
-      setCurrentPage(currentPage);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [currentPage]);
-
+  
   return (
     <>
       <Nav />
-      <Hero />
-      <Knowladge />
-      <Experiance />
-      <Section />
-      <Contact />
+      <Routes>
+        <Route exact path='/' element={<Main/>} />
+        <Route exact path='/work' element={<Work/>} />
+      </Routes>
       <Footer />
       <Cursor />
 
-      {/* Conditionally render page indicator */}
-      {currentPage !== 5 && (
-        <div className="page-indicator">
-          {[...Array(5).keys()].map((index) => (
-            <div
-              key={index}
-              className={`indicator-dot ${currentPage === index ? 'active' : ''} ${currentPage === 3 && index === 3 ? 'white-dot' : ''}`}
-            ></div>
-          ))}
-        </div>
-      )}
+      
     </>
   );
 }
