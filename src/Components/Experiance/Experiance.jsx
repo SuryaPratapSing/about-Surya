667bbbdf0d96c043064e44b73d73a6364562a251
surya <iamsingh.surya@gmail.com>
2024-04-18 15:23:16 +0530

    responsive

--- src/Components/Experiance/Experiance.jsx
+++ src/Components/Experiance/Experiance.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import tech from './tech.avif'
+import tech from './tech.png'
 
 const Experiance = () => {
   return (
-    <div className='w-full h-screen bg-[#4731d4]'>
-        <div className='w-4/5 h-full m-auto flex'>
-            <div className='w-2/5 h-full flex flex-col justify-center'>
-                <h1 className='font-extrabold text-7xl text-[#CCF381]'>Over the years,</h1>
+    <div className='w-full h-auto md:h-screen bg-[#4731d4] py-16 md:py-0'>
+        <div className='w-4/5 h-full m-auto md:flex pl-8 md:pl-0'>
+            <div className='w-full md:w-2/5 h-full flex flex-col justify-center'>
+                <h1 className='font-extrabold text-3xl md:text-7xl text-[#CCF381]'>Over the years,</h1>
                 <p className='mt-5 text-gray-50 text-sm leading-relaxed'>I've built products for companies and businesses around the globe ranging from marketing websites to complex solutions and enterprise apps with focus on fast, elegant and accessible user experiences.</p>
 
                 <p className='mt-5 text-gray-50 text-sm leading-relaxed'>Currently, I work at Shopify as a Senior Frontend Engineer crafting thoughtful and inclusive experiences that adhere to web standards for over 3 million merchants across the world.</p>
@@ -20,8 +20,8 @@ const Experiance = () => {
                 </p>
 
             </div>
-            <div className='w-3/5 h-full'>
-                <img src={tech}/>
+            <div className='w-full md:w-3/5 h-full flex justify-center items-center md:pl-20 mt-10 md:mt-0'>
+                <img className='w-full object-cover' src={tech}/>
             </div>
 
         </div>
--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
-import icon from './right.png'
 
 const Section = () => {
     return (
-        <div className='w-full h-screen flex justify-center bg-gray-50 pt-36 pb-28'>
-            <div className='w-11/12 h-full m-auto bg-white flex'>
-                <div className='w-1/2 h-full border-r-2 border-blue-400 pl-20 flex items-center'>
+        <div className='w-full h-auto md:h-screen flex justify-center bg-gray-50 pt-36 pb-28'>
+            <div className='w-11/12 h-full m-auto bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] rounded md:flex'>
+                <div className='w-full md:w-1/2 h-full border-b-2 md:border-b-0 md:border-r-2 border-blue-400 pl-20 flex items-center py-16 md:py-0'>
                     <div className='h-fit'>
-                        <h1 className='font-extrabold text-5xl text-[#4731d4] '>I build & <br />design stuff</h1>
-                        <p className='text-2xl font-light mt-10'>Open source <br /> projects, web apps<br /> and experimentals.</p>
-                        <button className='py-4 px-16 flex gap-5 border border-[#4731d4] rounded mt-10 items-center text-sm font-semibold text-[#4731d4] hover:bg-[#4731d4] hover:text-white transition-all duration-300 ease-in-out'>SEE MY WORKS <i class="fa-solid fa-arrow-right-long text-xl"></i></button>
-
-
-
-
+                        <h1 className='font-extrabold text-3xl md:text-5xl text-[#4731d4] '>I build & <br />design stuff</h1>
+                        <p className='md:text-2xl font-light mt-10'>Open source <br /> projects, web apps<br /> and experimentals.</p>
+                        <button className='py-2 md:py-4 px-6 md:px-16 flex gap-5 border border-[#4731d4] rounded mt-10 items-center text-xs md:text-sm font-semibold text-[#4731d4] hover:bg-[#4731d4] hover:text-white transition-all duration-300 ease-in-out'>SEE MY WORKS <i class="fa-solid fa-arrow-right-long text-xl"></i></button>
 
                     </div>
                 </div>
                 <div className='w-1/2 h-full pl-20 flex items-center'>
-                    <div className='h-fit'>
-                        <h1 className='font-extrabold text-5xl text-[#4731d4] '>I write,<br /> sometimes</h1>
-                        <p className='text-2xl font-light mt-10'>About design,
+                    <div className='h-fit py-16 md:py-0'>
+                        <h1 className='font-extrabold text-3xl md:text-5xl text-[#4731d4] '>I write,<br /> sometimes</h1>
+                        <p className='md:text-2xl font-light mt-10'>About design,
                             <br /> frontend dev,<br /> learning and life.</p>
-                        <button className='py-4 px-16 flex gap-5 border border-[#4731d4] rounded mt-10 items-center text-sm font-semibold text-[#4731d4] hover:bg-[#4731d4] hover:text-white transition-all duration-300 ease-in-out'>READ MY ARTICLES <i class="fa-solid fa-arrow-right-long text-xl"></i></button>
-
-
-
+                        <button className='py-2 md:py-4 px-6 md:px-16 flex gap-5 border border-[#4731d4] rounded mt-10 items-center text-xs md:text-sm font-semibold text-[#4731d4] hover:bg-[#4731d4] hover:text-white transition-all duration-300 ease-in-out'>READ MY ARTICLES <i class="fa-solid fa-arrow-right-long text-xl"></i></button>
 
                     </div>
                 </div>
